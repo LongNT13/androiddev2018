8a4a2a7ff9f6c0c8d85c94667e7792a3f828f2d0
DangVinhBao <sieusaoda@gmail.com>
2017-10-27 22:19:13 +0700

    Reupload base project: -Add MainActivity -Add News Feed fragment -Add OtherOptions fragment: a list of button -Remove default title bar -Warning update Android Studio to 3.0 before pull

---
+++ Group3Facebook/app/src/main/java/group3facebook/usth/edu/vn/group3facebook/MainActivity.java
@@ -0,0 +1,57 @@
+package group3facebook.usth.edu.vn.group3facebook;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        PagerAdapter adapter = new HomeFragmentPagerAdapter(getSupportFragmentManager());
+
+        ViewPager pager = (ViewPager)findViewById(R.id.pager);
+        pager.setOffscreenPageLimit(2);
+        pager.setAdapter(adapter);
+
+        TabLayout mainTab = (TabLayout)findViewById(R.id.tab);
+        mainTab.setupWithViewPager(pager);
+    }
+
+
+    public class HomeFragmentPagerAdapter extends FragmentPagerAdapter {
+        private final int PAGE_COUNT = 2;
+        private String titles[] = {"News Feeds", "Other"};
+
+        public HomeFragmentPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public int getCount() {
+            return PAGE_COUNT;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position){
+                case 0: return new NewsFeedFragment();
+                case 1: return new OtherOptionsFragment();
+            }
+            return new Fragment();
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return titles[position];
+        }
+    }
+}
